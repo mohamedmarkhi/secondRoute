c07d82f07656f21f1c437eac5f6b809cea5bd841
mohamedmarkhi <markhimohamed@outlook.fr>
2026-02-26 12:10:34 +0000

    create the first migration

--- database/migrations/2026_02_23_114550_create_articles_table.php
+++ database/migrations/2026_02_23_114550_create_articles_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("title");
+            $table->text('content');
+            $table->double("prix");
+            $table->timestamps();   
+
         });
     }
 
